959ee36b2709dd3d7d44313a404a05baae9d8829
i.samborskiy <samborskiy.ivan@gmail.com>
2018-08-17 16:34:48 +0800

    Add util class to count f-score, and implement algorithm to evaluate basement classifiers.

--- src/main/kotlin/sg/edu/nus/samborskii/App.kt
+++ src/main/kotlin/sg/edu/nus/samborskii/App.kt
@@ -2,31 +2,51 @@ package sg.edu.nus.samborskii
 
 import sg.edu.nus.samborskii.g2d.G2DReader
 import sg.edu.nus.samborskii.g2d.toInstances
+import weka.classifiers.Classifier
 import weka.classifiers.Evaluation
+import weka.classifiers.bayes.NaiveBayes
+import weka.classifiers.functions.LibSVM
+import weka.classifiers.lazy.IBk
+import weka.classifiers.trees.J48
 import weka.classifiers.trees.RandomForest
+import weka.filters.Filter
+import weka.filters.unsupervised.attribute.Normalize
 import java.util.*
 
+private const val NUM_FOLDS = 10
+
+private val CLASSIFIERS_BUILDER: Map<() -> Classifier, String> = mapOf(
+        { IBk() } to "kNN",
+        { NaiveBayes() } to "NaiveBayes",
+        { J48() } to "DecisionTree",
+        { LibSVM() } to "SVM",
+        { RandomForest() } to "RandomForest"
+)
+
+private val READER = G2DReader()
+private val DATA: Map<() -> Map<Int, List<Int>>, String> = mapOf(
+        { READER.genresData() } to "genres",
+        { READER.genresTimeData() } to "genres_time",
+        { READER.gamesData() } to "games",
+        { READER.gamesTimeData() } to "games_time"
+)
+private val GT = READER.groundTruth()
+private val CLASS_NUM = GT.values.distinct().count()
 
 fun main(args: Array<String>) {
-    val reader = G2DReader()
+    DATA.map { (dataReader, label) ->
+        var instances = toInstances(dataReader(), GT)
+        val filter = Normalize().apply { setInputFormat(instances) }
+        instances = Filter.useFilter(instances, filter)
 
-    val data = mapOf(
-            reader.genresData() to "genres",
-            reader.genresTimeData() to "genres_time",
-            reader.gamesData() to "games",
-            reader.gamesTimeData() to "games_time"
-    )
-    val gt = reader.groundTruth()
-
-    data.map { (dataMap, label) ->
-        val instances = toInstances(dataMap, gt)
-
-        val classifier = RandomForest()
-
-        val evaluation = Evaluation(instances)
-        evaluation.crossValidateModel(classifier, instances, 10, Random(1))
-        println(label)
-        println(evaluation.getFMeasure(2))
+        println("--- $label ---")
+        CLASSIFIERS_BUILDER.forEach { (builder, name) ->
+            val classifier = builder()
+            val evaluation = Evaluation(instances)
+            evaluation.crossValidateModel(classifier, instances, NUM_FOLDS, Random(1))
+            println(name)
+            println(evaluation.getFMeasure(CLASS_NUM))
+        }
         println()
     }
 }
--- src/main/kotlin/sg/edu/nus/samborskii/WekaUtils.kt
+++ src/main/kotlin/sg/edu/nus/samborskii/WekaUtils.kt
@@ -2,12 +2,14 @@ package sg.edu.nus.samborskii
 
 import weka.classifiers.Evaluation
 
-fun Evaluation.getPrecision(numClasses: Int): Double = (0 until numClasses).map { precision(it) }.average()
+fun Evaluation.getPrecision(numClasses: Int): Double = (0 until numClasses).map { precision(it) }.mapNaNto0().average()
 
-fun Evaluation.getRecall(numClasses: Int): Double = (0 until numClasses).map { recall(it) }.average()
+fun Evaluation.getRecall(numClasses: Int): Double = (0 until numClasses).map { recall(it) }.mapNaNto0().average()
 
 fun Evaluation.getFMeasure(numClasses: Int): Double {
     val precision = getPrecision(numClasses)
     val recall = getRecall(numClasses)
     return 2 * precision * recall / (precision + recall)
 }
+
+private fun List<Double>.mapNaNto0(): List<Double> = map { if (it.isNaN()) 0.0 else it }
